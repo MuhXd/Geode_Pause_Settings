c84dbb94f060799a0b6023ba0cfa6a7d2df4d0e7
MuhXd <momo2010sofia@gmail.com>
2024-01-27 18:04:49 +0000

    dfkidijosohijgfohij

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/ClassName.hpp>
+//#include <Geode/modify/GameOptionsLayer.hpp>
 #include <Geode/binding/GameOptionsLayer.hpp>
+#include <Geode/binding/GJBaseGameLayer.hpp>
+
 
 using namespace geode::prelude;
 
-class $modify(GameOptionsLayer) {
+class $modify(GJBaseGameLayer) {
 	 bool init() {
-        if (!GameOptionsLayer::init())
+		if (!GJBaseGameLayer::init())
             return false;
+			
 		FLAlertLayer::create("Pause Test", "Hello from my custom mod!", "YAY!")->show(); 
 	 }
 };
